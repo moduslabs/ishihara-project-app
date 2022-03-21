af05bf1e2b1b278bf92fe9a3effda1034e0d46da
Ayomide Bakare <bakkareayomideo@gmail.com>
2022-03-21 16:18:29 +0300

    FET-141 update results page

--- src/pages/confirmation-page/confirmation-page.tsx
+++ src/pages/confirmation-page/confirmation-page.tsx
@@ -1,4 +1,5 @@
 import { Component, h, State } from '@stencil/core';
+import cx from 'classnames';
 import state from '../../store';
 
 @Component({
@@ -6,9 +7,11 @@ import state from '../../store';
   styleUrl: 'confirmation-page.css',
 })
 export class ConfirmationPage {
-  @State() score = state.plates.filter((plate) => plate.key === plate.answer)
+  @State() score = state.plates.filter(plate => plate.key === plate.answer);
 
   render() {
+    console.log('Scores', this.score);
+    console.log('Plates', state.plates);
     return [
       <ion-header>
         <ion-toolbar color="primary">
@@ -24,10 +27,37 @@ export class ConfirmationPage {
           <img src="/assets/images/cover-isihara.png" alt="Ishihara" />
           <div class="report">
             <div class="circle">
-              <span class="score">{this.score.length}/{state.plates.length}</span>
+              <span class="score">
+                {this.score.length}/{state.plates.length}
+              </span>
             </div>
           </div>
         </div>
+        <ion-grid>
+          <ion-row class="header-row">
+            <ion-col size="4">Plate</ion-col>
+            <ion-col size="4">Your Answer</ion-col>
+            <ion-col size="4">Correct</ion-col>
+          </ion-row>
+          {state.plates?.map(({ answer, key }, index) => {
+            return (
+              <ion-row>
+                <ion-col size="4">{index + 1}</ion-col>
+                <ion-col
+                  size="4"
+                  class={cx({
+                    'no-answer': !answer,
+                    'flawed': key !== answer,
+                    'flawless': key === answer,
+                  })}
+                >
+                  {answer || '(no answer)'}
+                </ion-col>
+                <ion-col size="4">{key}</ion-col>
+              </ion-row>
+            );
+          })}
+        </ion-grid>
       </ion-content>,
     ];
   }
