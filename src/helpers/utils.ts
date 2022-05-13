f4b86f17d0c2b58b7c8239f6bf6addc678f6d533
Overrideveloper <bansowisdom@gmail.com>
2022-05-13 19:25:06 +0100

    feat(FET-155): Add restrictions to answers input
    - Restrict input to alphanumeric characters
    - Require an answer to be provided before going to next slide (either by swiping or clicking next)
    - Add "skip" button to move to next slide without providing an answer
    - Change next button text from "next" to "finish" when on the last slide

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,18 +1,22 @@
 import { Plate } from '../types/plate';
 import state from '../store';
 
-export const capitalizePlateAnswer = (plates: Plate[]) => {
+const preRenderFirstPlateImage = (url: string): void => {
+  const img = document.createElement('img');
+  img.src = url;
+};
+
+export const capitalizePlateAnswer = (plates: Plate[]): Plate[] => {
   return plates.map(p => ({ ...p, answer: p.answer ? String(p.answer).toUpperCase() : p.answer }));
 };
 
-const preRenderFristPlateImage = (url) => {
-  const img = document.createElement('img');
-  img.src = url
-};
-
-export async function loadPlates() {
+export async function loadPlates(): Promise<void> {
   const response = await fetch('https://b9jdjjz440.execute-api.us-east-1.amazonaws.com/test/plates?limit=15');
   const plates = await response.json();
   state.plates = plates ? plates?.map(plate => ({ ...plate, answer: null })) : null;
-  preRenderFristPlateImage(state.plates[0].url);
+  preRenderFirstPlateImage(state.plates[0].url);
+}
+
+export function assertOnlyAlphanumericChars(value: string): boolean {
+  return /^[a-zA-Z0-9]+$/.test(value);
 }
