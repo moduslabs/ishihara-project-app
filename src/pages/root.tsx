c601bcae7d978928ad52cf79467303e2c017d06a
Ayomide Bakare <bakkareayomideo@gmail.com>
2022-05-31 14:00:39 +0300

    FET-621 manage history due to ion-back-button issue

--- src/pages/root.tsx
+++ src/pages/root.tsx
@@ -2,7 +2,7 @@ import { Component, h } from '@stencil/core';
 import { SplashScreen } from '@capacitor/splash-screen';
 import routes from '../helpers/routes';
 import { loadPlates } from '../helpers/utils';
-
+import state from '../store';
 @Component({
   tag: 'app-root',
 })
@@ -14,6 +14,7 @@ export class AppRoot {
 
   async handlePageEnter(e) {
     const { to = routes.home.url, from } = e.detail;
+    state.pushHistory = { current: to, previous: from };
     if (from && to === routes.home.url) {
       await loadPlates();
     }
--- src/store.ts
+++ src/store.ts
@@ -2,10 +2,27 @@ import { createStore } from '@stencil/store';
 
 const { state, onChange } = createStore({
   plates: [],
+  history: [],
+  pushHistory: {
+    current: null,
+    previous: null,
+  },
 });
 
 onChange('plates', value => {
   state.plates = value;
+});
+
+onChange('pushHistory', ({ current, previous }) => {
+  // maintain linked history stack
+  if (previous && current) {
+    const pairIdx = state.history.findIndex(h => h.previous === current && h.current === previous);
+    if (pairIdx === 0) {
+      state.history = state.history.slice(1, state.history.length);
+    } else {
+      state.history = [{ previous, current }, ...state.history];
+    }
+  }
 });
 
 export default state;
